9d2fff05d0fda0e422d0c4e030f2fd3a0d9fdb57
Florian Bruhin <git@the-compiler.org>
2015-02-03 19:01:44 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,24 +2,24 @@
 
 set -e
 dir="$PWD"
-outdir=/srv/http/qutebrowser/qt-debug
+outdir=/srv/http/qutebrowser/qt-debug/x86_64
 
-export DEBUG_CFLAGS='-ggdb3 -fvar-tracking-assignments -Og'
-export DEBUG_CXXFLAGS='-ggdb3 -fvar-tracking-assignments -Og'
-export PKGEXT='.pkg.tar.xz'
-export BUILDENV=(!distcc color ccache check sign)
-export GPGKEY="0xE80A0C82"
+if [[ $1 == repackage ]]; then
+    makepkg_opts=('-siR' '--config' "$dir/makepkg.conf")
+else
+    makepkg_opts=('-si' '--config' "$dir/makepkg.conf")
+fi
 
 cd "$dir/qt5"
 rm *.pkg.tar* || true
 rm -r src/python2-path || true
-makepkg -si
-cp *.pkg.tar.xz "$outdir"
+makepkg "${makepkg_opts[@]}"
+mv *.pkg.tar.xz* "$outdir"
 
 cd "$dir/pyqt5"
 rm *.pkg.tar* || true
-makepkg -si
-cp *.pkg.tar.xz "$outdir"
+makepkg "${makepkg_opts[@]}"
+mv *.pkg.tar.xz* "$outdir"
 
 repo-add "$outdir/qt-debug.db.tar.gz" "$outdir"/*.pkg.tar.xz
 
