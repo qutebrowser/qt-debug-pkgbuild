9dd8908d67ace89228b965bf12d811b904c2c15f
Florian Bruhin <git@the-compiler.org>
2016-03-31 13:26:47 +0200

    Add patch.sh

---
+++ patch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+for pkg in qt5-*; do
+    # pkgname
+    sed -i 's|pkgname=\(.*\)|pkgname=\1-debug\n_orig_pkgname=${pkgname/-debug/}|' $pkg/PKGBUILD
+    sed -i 's|_pkgfqn="${pkgname/5-/}-opensource-src-${_qtver}"|_pkgfqn="${_orig_pkgname/5-/}-opensource-src-${_qtver}"|' $pkg/PKGBUILD
+
+    # conflicts
+    grep -q conflicts $pkg/PKGBUILD || exit 1
+    sed -i 's|conflicts=(\(.*\))|conflicts=(\1 '\'$pkg\'')|' $pkg/PKGBUILD
+
+    # provides
+    grep -q provides $pkg/PKGBUILD && exit 1
+    sed -i '/^conflicts=/aprovides=("'$pkg'==$pkgver")' $pkg/PKGBUILD
+    grep -q provides $pkg/PKGBUILD || exit 1
+
+    # options
+    sed -i '/^provides=/aoptions=("debug" "!strip")' $pkg/PKGBUILD
+    grep -q options $pkg/PKGBUILD || exit 1
+done
+
+# -force-debug-info
+sed -i 's/${SSE2}/& \\\n    -force-debug-info/' qt5-base/PKGBUILD
