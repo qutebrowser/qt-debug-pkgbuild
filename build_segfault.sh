2655008f0df4278f2284a171f4df008431059721
Florian Bruhin <git@the-compiler.org>
2016-04-13 10:30:10 +0200

    Try to not clobber existing packages

--- build_segfault.sh
+++ build_segfault.sh
@@ -8,13 +8,25 @@ qtbuilddir="~/docker/archlinux-qtbuild"
 ssh segfault mkdir -p "$qtbuilddir"
 rsync -avPh --files-from=<(git ls-files) . segfault:"$qtbuilddir"
 ssh segfault "$qtbuilddir/build_segfault_docker.sh" "$@"
-rm "$outdir"/*
-rsync -avPh segfault:"$qtbuilddir/out/" "$outdir"
 
-for f in "$outdir"/*.pkg.tar.xz; do
-    gpg --detach-sign --default-key 0xE80A0C82 "$f"
+if (( $# > 1 )); then
+    packages=$(cat pkglist)
+else
+    packages=$@
+fi
+
+for pkg in $packages; do
+    #repo-remove "$outdir/qt-debug.db.tar.gz" "$pkg"
+
+    rm "$outdir"/$pkg-*
+
+    rsync -avPh segfault:"$qtbuilddir/out/" "$outdir"
+
+    for f in "$outdir"/$pkg-*.pkg.tar.xz; do
+        gpg --detach-sign --default-key 0xE80A0C82 "$f"
+    done
+
+    repo-add "$outdir/qt-debug.db.tar.gz" "$outdir/$pkg-"*.pkg.tar.xz
 done
-
-repo-add "$outdir/qt-debug.db.tar.gz" "$outdir"/*.pkg.tar.xz
 
 cd "$dir"
