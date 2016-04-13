a74dc7a11e4335f94e488e9ed33c6fb549884930
Florian Bruhin <git@the-compiler.org>
2016-04-13 10:55:47 +0200

    Make sure we use -f

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -8,5 +8,5 @@ for pkg in $PACKAGES; do
     cd ~/$pkg
     # Remove the thing we're building so we can install -debug easily.
     sudo pacman -Rdd --noconfirm $pkg
-    makepkg -i --noconfirm
+    makepkg -i -f --noconfirm
 done
