838089f3128ca2e8c34cb67e3881bdab8fe57a46
Florian Bruhin <git@the-compiler.org>
2016-04-27 15:29:22 +0200

    Add --temp to build_segfault.sh

--- build_segfault.sh
+++ build_segfault.sh
@@ -2,8 +2,16 @@
 
 set -e
 dir="$PWD"
-outdir=/srv/http/qutebrowser/qt-debug/x86_64
 qtbuilddir="~/docker/archlinux-qtbuild"
+
+if [[ $1 == --temp ]]; then
+    outdir=$PWD/out
+    temp=1
+    shift 1
+else
+    outdir=/srv/http/qutebrowser/qt-debug/x86_64
+    temp=0
+fi
 
 ssh segfault mkdir -p "$qtbuilddir"
 rsync -avPh --files-from=<(git ls-files) . segfault:"$qtbuilddir"
@@ -19,11 +27,13 @@ for pkg in $packages; do
     rm -f "$outdir"/$pkg-*
     rsync -avPh segfault:"$qtbuilddir/out/${pkg}-*" "$outdir"
 
-    for f in "$outdir"/$pkg-*.pkg.tar.xz; do
-        gpg --detach-sign --default-key 0xE80A0C82 "$f"
-    done
+    if (( ! temp )); then
+        for f in "$outdir"/$pkg-*.pkg.tar.xz; do
+            gpg --detach-sign --default-key 0xE80A0C82 "$f"
+        done
 
-    repo-add "$outdir/qt-debug.db.tar.gz" "$outdir/$pkg-"*.pkg.tar.xz
+        repo-add "$outdir/qt-debug.db.tar.gz" "$outdir/$pkg-"*.pkg.tar.xz
+    fi
 done
 
 cd "$dir"
