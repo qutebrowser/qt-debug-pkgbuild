248d5f4dc5b392b9cedd4faca7ebbf4d0188c006
Florian Bruhin <git@the-compiler.org>
2016-04-28 11:20:45 +0200

    Fix package removing for pyqt5

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,7 +6,10 @@ set -e
 
 for pkg in $PACKAGES; do
     cd ~/$pkg
+
     # Remove the thing we're building so we can install -debug easily.
+    [[ $pkg == pyqt5 ]] && pkg="pyqt5-common python-pyqt5 python2-pyqt5"
     sudo pacman -Rdd --noconfirm $pkg
+
     makepkg -i -f --noconfirm
 done
