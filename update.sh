95b5a3b376a8e1c3024cfaa84de81a6972a07caf
Florian Bruhin <git@the-compiler.org>
2016-06-24 23:46:32 +0200

    update.sh: Allow to specify package names

--- update.sh
+++ update.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-rm -rf pkglist qt5-* pyqt5
+if (( $# == 0 )); then
+    rm -f pkglist
+    for name in dep opt; do
+        echo "Downloading $name package list..."
+        wget -q -O- "https://projects.archlinux.org/kde-build.git/plain/qt-${name}-packages?h=qt" | \
+            grep -vE '(^#|^$)' >> pkglist
+    done
 
-for name in dep opt; do
-    echo "Downloading $name package list..."
-    wget -q -O- "https://projects.archlinux.org/kde-build.git/plain/qt-${name}-packages?h=qt" | \
-        grep -vE '(^#|^$)' >> pkglist
-done
+    echo qt5-webkit >> pkglist
+    echo pyqt5 >> pkglist
 
-echo qt5-webkit >> pkglist
-echo pyqt5 >> pkglist
+    mapfile -t packages < pkglist
+else
+    packages=$@
+fi
 
-rm -rf packages
+rm -rf packages "${packages[@]}"
 
-while read pkg; do
+for pkg in "${packages[@]}"; do
     echo "Downloading $pkg..."
     git clone git://projects.archlinux.org/svntogit/packages.git \
         --branch=packages/$pkg --single-branch -q || exit 1
     cp -R packages/trunk $pkg || exit 1
-    rm -r packages
-done < pkglist
+    rm -rf packages
+done
