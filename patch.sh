f8531414ce0202f0d9b939705581de2d45b17041
Florian Bruhin <git@the-compiler.org>
2016-06-24 23:52:14 +0200

    Improve patch.sh

--- patch.sh
+++ patch.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
 for pkg in qt5-*; do
     # pkgname
+    if grep -q _orig_pkgname $pkg/PKGBUILD; then
+        echo "$pkg was already rewritten?"
+        exit 1
+    fi
     sed -i 's|pkgname=\(.*\)|pkgname=\1-debug\n_orig_pkgname=${pkgname/-debug/}|' $pkg/PKGBUILD
     sed -i 's|_pkgfqn="${pkgname/5-/}-\(.*\)"|_pkgfqn="${_orig_pkgname/5-/}-\1"|' $pkg/PKGBUILD
 
     # conflicts
-    grep -q conflicts $pkg/PKGBUILD || exit 1
-    sed -i 's|conflicts=(\(.*\))|conflicts=(\1 '\'$pkg\'')|' $pkg/PKGBUILD
+    if grep -q conflicts $pkg/PKGBUILD; then
+        sed -i 's|conflicts=(\(.*\))|conflicts=(\1 '\'$pkg\'')|' $pkg/PKGBUILD
+    else
+        grep -q depends= $pkg/PKGBUILD || exit 1
+        sed -i '/^depends=/aconflicts=('\'$pkg\'')' $pkg/PKGBUILD
+    fi
 
     # provides
     grep -q provides $pkg/PKGBUILD && exit 1
-    sed -i '/^conflicts=/aprovides=("'$pkg'==$pkgver")' $pkg/PKGBUILD
+    sed -i '/^depends=/aprovides=("'$pkg'==$pkgver")' $pkg/PKGBUILD
     grep -q provides $pkg/PKGBUILD || exit 1
 
     # options
