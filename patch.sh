72571ee6e7d76af89204f8d405016d520cd5b1bc
Florian Bruhin <git@the-compiler.org>
2016-07-01 10:49:11 +0200

    patch.sh: Also patch pyqt5

--- patch.sh
+++ patch.sh
@@ -40,6 +40,40 @@ for pkg in qt5-*; do
     grep -q options $pkg/PKGBUILD || fail
 done
 
-#### qt5-base patches
+### qt5-base patches
 sed -i 's/${SSE2}/& \\\n    -force-debug-info/' qt5-base/PKGBUILD
 grep -q -- -force-debug-info qt5-base/PKGBUILD || fail
+
+### pyqt5 patches
+# replace all qt5 references
+sed -i 's/\(qt5-[a-z0-9-]*\)/\1-debug/g' pyqt5/PKGBUILD
+# replace all pyqt5 references
+sed -i 's/\(pyqt5[a-z0-9-]*\)/\1-debug/g' pyqt5/PKGBUILD
+# replace all pyqt5 references
+# add debug options
+sed -i '/^provides=/aoptions=("debug" "!strip")' pyqt5/PKGBUILD
+grep -q options $pkg/PKGBUILD || fail
+# add debug switch
+sed -i 's|-q /usr/bin/qmake-qt5|& \\\n    --debug|' pyqt5/PKGBUILD
+grep -q -- --debug pyqt5/PKGBUILD || fail
+
+# add provides/conflicts/options sections to package functions
+line1='provides=("pyqt5-common=$pkgver")'
+line2='conflicts=("pyqt5-common")'
+line3='options=("debug" "!strip")'
+sed -i "/^package_pyqt5-common-debug/a$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
+grep -q "$line1" pyqt5/PKGBUILD || fail
+grep -q "$line2" pyqt5/PKGBUILD || fail
+grep -q "$line3" pyqt5/PKGBUILD || fail
+
+line1='provides=("python-pyqt5=$pkgver")'
+line2='conflicts=("python-pyqt5")'
+sed -i "/^package_python-pyqt5-debug/a$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
+grep -q "$line1" pyqt5/PKGBUILD || fail
+grep -q "$line2" pyqt5/PKGBUILD || fail
+
+line1='provides=("python2-pyqt5=$pkgver")'
+line2='conflicts=("python2-pyqt5")'
+sed -i "/^package_python2-pyqt5-debug/a$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
+grep -q "$line1" pyqt5/PKGBUILD || fail
+grep -q "$line2" pyqt5/PKGBUILD || fail
