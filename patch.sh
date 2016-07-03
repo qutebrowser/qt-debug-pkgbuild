434568cc11135172676296febd4d18f21855fff7
Florian Bruhin <git@the-compiler.org>
2016-07-03 16:45:54 +0200

    patch.sh: Accept package names

--- patch.sh
+++ patch.sh
@@ -7,68 +7,83 @@ fail() {
     exit 1
 }
 
-### First handle all qt5-* packages
-for pkg in qt5-*; do
-    if grep -q _orig_pkgname $pkg/PKGBUILD; then
-        echo "$pkg was already rewritten?"
-        exit 1
-    fi
+patch_qt() {
+    for pkg in "$@"; do
+        [[ $pkg != *qt5-* ]] && continue
 
-    # Adjust package names
-    sed -i 's|^pkgname=.*|&-debug\n_orig_pkgname=${pkgname/-debug/}|' $pkg/PKGBUILD
-    sed -i '/^_pkgfqn=/s/pkgname/_orig_pkgname/g' $pkg/PKGBUILD
-    sed -i 's|/usr/share/licenses/qt5-base|&-debug|g' $pkg/PKGBUILD
+        if grep -q _orig_pkgname $pkg/PKGBUILD; then
+            echo "$pkg was already rewritten?"
+            exit 1
+        fi
 
-    # add conflicts-entry for non-debug package
-    if grep -q conflicts $pkg/PKGBUILD; then
-        sed -i 's|conflicts=(\(.*\))|conflicts=(\1 '\'$pkg\'')|' $pkg/PKGBUILD
-    else
-        grep -q _pkgfqn= $pkg/PKGBUILD || fail
-        sed -i '/^_pkgfqn=/aconflicts=('\'$pkg\'')' $pkg/PKGBUILD
-    fi
+        # Adjust package names
+        sed -i 's|^pkgname=.*|&-debug\n_orig_pkgname=${pkgname/-debug/}|' $pkg/PKGBUILD
+        sed -i '/^_pkgfqn=/s/pkgname/_orig_pkgname/g' $pkg/PKGBUILD
+        sed -i 's|/usr/share/licenses/qt5-base|&-debug|g' $pkg/PKGBUILD
 
-    # add provides-entry for non-debug package
-    grep -q provides $pkg/PKGBUILD && fail
-    sed -i '/^_pkgfqn=/aprovides=("'$pkg'==$pkgver")' $pkg/PKGBUILD
-    grep -q provides $pkg/PKGBUILD || fail
+        # add conflicts-entry for non-debug package
+        if grep -q conflicts $pkg/PKGBUILD; then
+            sed -i 's|conflicts=(\(.*\))|conflicts=(\1 '\'$pkg\'')|' $pkg/PKGBUILD
+        else
+            grep -q _pkgfqn= $pkg/PKGBUILD || fail
+            sed -i '/^_pkgfqn=/aconflicts=('\'$pkg\'')' $pkg/PKGBUILD
+        fi
 
+        # add provides-entry for non-debug package
+        grep -q provides $pkg/PKGBUILD && fail
+        sed -i '/^_pkgfqn=/aprovides=("'$pkg'==$pkgver")' $pkg/PKGBUILD
+        grep -q provides $pkg/PKGBUILD || fail
+
+        # add debug options
+        sed -i '/^provides=/aoptions=("debug" "!strip")' $pkg/PKGBUILD
+        grep -q options $pkg/PKGBUILD || fail
+    done
+}
+
+patch_qt_base() {
+    sed -i 's/${SSE2}/& \\\n    -force-debug-info/' qt5-base/PKGBUILD
+    grep -q -- -force-debug-info qt5-base/PKGBUILD || fail
+}
+
+patch_pyqt() {
+    # replace all pyqt5 references
+    sed -i 's/\(pyqt5[a-z0-9-]*\)/\1-debug/g' pyqt5/PKGBUILD
+    sed -i 's/-debug\.so/\.so/g' pyqt5/PKGBUILD
     # add debug options
-    sed -i '/^provides=/aoptions=("debug" "!strip")' $pkg/PKGBUILD
+    sed -i '/^md5sums=/aoptions=("debug" "!strip")' pyqt5/PKGBUILD
     grep -q options $pkg/PKGBUILD || fail
-done
+    # add debug switch
+    sed -i 's|-q /usr/bin/qmake-qt5|& \\\n    --debug|' pyqt5/PKGBUILD
+    grep -q -- --debug pyqt5/PKGBUILD || fail
 
-### qt5-base patches
-sed -i 's/${SSE2}/& \\\n    -force-debug-info/' qt5-base/PKGBUILD
-grep -q -- -force-debug-info qt5-base/PKGBUILD || fail
+    # add provides/conflicts/options sections to package functions
+    line1='  provides=("pyqt5-common=$pkgver")'
+    line2='  conflicts=("pyqt5-common")'
+    line3='  options=("debug" "!strip")'
+    sed -i "/^package_pyqt5-common-debug/a\\$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
+    grep -q "$line1" pyqt5/PKGBUILD || fail
+    grep -q "$line2" pyqt5/PKGBUILD || fail
+    grep -q "$line3" pyqt5/PKGBUILD || fail
 
-### pyqt5 patches
-# replace all pyqt5 references
-sed -i 's/\(pyqt5[a-z0-9-]*\)/\1-debug/g' pyqt5/PKGBUILD
-sed -i 's/-debug\.so/\.so/g' pyqt5/PKGBUILD
-# add debug options
-sed -i '/^md5sums=/aoptions=("debug" "!strip")' pyqt5/PKGBUILD
-grep -q options $pkg/PKGBUILD || fail
-# add debug switch
-sed -i 's|-q /usr/bin/qmake-qt5|& \\\n    --debug|' pyqt5/PKGBUILD
-grep -q -- --debug pyqt5/PKGBUILD || fail
+    line1='  provides=("python-pyqt5=$pkgver")'
+    line2='  conflicts=("python-pyqt5")'
+    sed -i "/^package_python-pyqt5-debug/a\\$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
+    grep -q "$line1" pyqt5/PKGBUILD || fail
+    grep -q "$line2" pyqt5/PKGBUILD || fail
 
-# add provides/conflicts/options sections to package functions
-line1='  provides=("pyqt5-common=$pkgver")'
-line2='  conflicts=("pyqt5-common")'
-line3='  options=("debug" "!strip")'
-sed -i "/^package_pyqt5-common-debug/a\\$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
-grep -q "$line1" pyqt5/PKGBUILD || fail
-grep -q "$line2" pyqt5/PKGBUILD || fail
-grep -q "$line3" pyqt5/PKGBUILD || fail
+    line1='  provides=("python2-pyqt5=$pkgver")'
+    line2='  conflicts=("python2-pyqt5")'
+    sed -i "/^package_python2-pyqt5-debug/a\\$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
+    grep -q "$line1" pyqt5/PKGBUILD || fail
+    grep -q "$line2" pyqt5/PKGBUILD || fail
+}
 
-line1='  provides=("python-pyqt5=$pkgver")'
-line2='  conflicts=("python-pyqt5")'
-sed -i "/^package_python-pyqt5-debug/a\\$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
-grep -q "$line1" pyqt5/PKGBUILD || fail
-grep -q "$line2" pyqt5/PKGBUILD || fail
+if (( $# == 0 )); then
+    packages=(qt5-* pyqt5)
+else
+    packages="$@"
+fi
 
-line1='  provides=("python2-pyqt5=$pkgver")'
-line2='  conflicts=("python2-pyqt5")'
-sed -i "/^package_python2-pyqt5-debug/a\\$line1\\n$line2\\n$line3" pyqt5/PKGBUILD
-grep -q "$line1" pyqt5/PKGBUILD || fail
-grep -q "$line2" pyqt5/PKGBUILD || fail
+patch_qt "${packages[@]}"
+[[ "${packages[@]}" == *qt5-base* ]] && patch_qt_base
+[[ "${packages[@]}" == *pyqt5* ]] && patch_pyqt
