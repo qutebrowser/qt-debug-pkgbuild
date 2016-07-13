72ef49080fda2c913126cd4f6465c5fdd42f4d98
Florian Bruhin <git@the-compiler.org>
2016-07-13 15:51:07 +0200

    docker_entrypoint: Always build qt5-base

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,6 +4,14 @@ set -e
 
 [[ ! $PACKAGES ]] && PACKAGES=$(cat pkglist)
 
+# Always build qt5-base so we can be sure we have debugging symbol settings in
+# QT_CONFIG for other modules
+if [[ $PACKAGES != *qt5-base* ]]; then
+    cd ~/qt5-base
+    sudo pacman -Rdd --noconfirm qt5-base
+    makepkg -i -f --noconfirm
+fi
+
 for pkg in $PACKAGES; do
     cd ~/$pkg
 
