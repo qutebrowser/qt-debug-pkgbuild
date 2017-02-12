933f015c233fe37003f77f6af17182e835f20ca2
Florian Bruhin <git@the-compiler.org>
2017-02-12 01:26:43 +0100

    Add qt5-webkit-ng
    See #11

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ if (( $# == 0 )); then
     done
 
     grep -q qt5-webkit pkglist || echo qt5-webkit >> pkglist
+    echo qt5-webkit-ng >> pkglist
     echo pyqt5 >> pkglist
 
     mapfile -t packages < pkglist
