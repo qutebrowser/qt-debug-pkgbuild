65e28399235e8af100e2f1cec21d24eaca75ddb7
Florian Bruhin <git@the-compiler.org>
2019-06-30 23:10:55 +0200

    Remove qt5-doc
    See #20

--- update.sh
+++ update.sh
@@ -11,6 +11,7 @@ if (( $# == 0 )); then
     grep -q qt5-webkit pkglist || echo qt5-webkit >> pkglist
     echo pyqt5 >> pkglist
     echo pyqtwebengine >> pkglist
+    sed -i '/qt5-doc/d' pkglist
 
     mapfile -t packages < pkglist
 else
