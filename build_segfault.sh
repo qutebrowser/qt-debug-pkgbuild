d6264a5d760a824e7d9c6298124bd49d58f71e78
Florian Bruhin <git@the-compiler.org>
2019-07-05 16:58:13 +0200

    Generate signatures even without --temp

--- build_segfault.sh
+++ build_segfault.sh
@@ -28,11 +28,11 @@ for pkg in $packages; do
     rm -f "$outdir"/*$pkg-*
     rsync -avPh segfault:"$qtbuilddir/out/*${pkg}-*" "$outdir"
 
-    if (( ! temp )); then
-        for f in "$outdir"/*$pkg-*.pkg.tar.xz; do
-            gpg --detach-sign --default-key 0xE80A0C82 "$f"
-        done
+    for f in "$outdir"/*$pkg-*.pkg.tar.xz; do
+        gpg --detach-sign --default-key 0xE80A0C82 "$f"
+    done
 
+    if (( ! temp )); then
         repo-add "$outdir/qt-debug.db.tar.gz" "$outdir"/*"$pkg"-*.pkg.tar.xz
     fi
 done
